b8444b26445cfa9355cfe9bd9fc65cce635b7c1e
Danily dos Santos Baroni <140064539+dandannyy@users.noreply.github.com>
2026-05-15 19:49:34 -0300

    Warmup.py

--- Warmup.py
+++ Warmup.py
@@ -1,5 +1,6 @@
 from karel.stanfordkarel import *
 
+# Week 1
 """
 The warmup program defines a "main" function which currently just has oneCommand. 
 Add two more commands to make karel: move(), pick_beeper(), move()
