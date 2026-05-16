d598ad7ab8882e0acff5bd45d5a18238e53eba38
Danily dos Santos Baroni <140064539+dandannyy@users.noreply.github.com>
2026-05-16 13:08:03 -0300

    The_Game_of_Nimm.py

--- The_Game_of_Nimm.py
+++ The_Game_of_Nimm.py
@@ -1,4 +1,4 @@
-# Week 4(OPTIONAL)
+# Week 4 (OPTIONAL)
 """
 Nimm is an ancient game of strategy that is named after the old German word for "take." It is also called Tiouk Tiouk in West Africa and Tsynshidzi in China. 
 Players alternate taking stones until there are zero left. The game of Nimm goes as follows:
