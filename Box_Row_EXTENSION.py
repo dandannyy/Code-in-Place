a3f82806e8654f5dec1ea12145aaefefc57c7952
Danily dos Santos Baroni <140064539+dandannyy@users.noreply.github.com>
2026-05-23 17:09:48 -0300

    Update Box_Row_EXTENSION.py

--- Box_Row_EXTENSION.py
+++ Box_Row_EXTENSION.py
@@ -12,18 +12,19 @@ BOX_SIZE = CANVAS_WIDTH / N_BOXES
 
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-    for i in range(N_BOXES):
-        left_x = i * BOX_SIZE 
-        top_y = CANVAS_HEIGHT - BOX_SIZE # will only occupy half
-        right_x = left_x + BOX_SIZE # will add the boxes
-        bottom_y = CANVAS_HEIGHT # start from bottom
-        canvas.create_rectangle(
-            left_x, 
-            top_y, 
-            right_x, 
-            bottom_y, 
-            "white", 
-            "black")
+    for i in range(N_BOXES): # for loops to rows
+        for f in range(N_BOXES): # for loops to columns
+            left_x = i * BOX_SIZE # will repeat rows
+            top_y = f * BOX_SIZE # will repeat columns
+            right_x = left_x + BOX_SIZE # will add the boxes
+            bottom_y = top_y + BOX_SIZE # will add the boxes
+            canvas.create_rectangle(
+                left_x, 
+                top_y, 
+                right_x, 
+                bottom_y, 
+                "white", 
+                "black")
 
 if __name__ == '__main__':
     main()
